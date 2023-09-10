fd9288ddebb9dd086ab7c8cf41749f91308e3ef9
Rajiv-Bhandari <bhandas.3282@gmail.com>
2023-09-11 00:22:48 +0545

    show product data in home page and pagination

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,24 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Product;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.userpage');
+        $product = Product::paginate(9);
+        // $product = Product::all();
+        return view('home.userpage',compact('product'));
     }
     public function redirect()
     {
         $usertype = Auth::user()->usertype;
-
+        $product = Product::paginate(9);
         if($usertype == '1')
         {
-            return view('admin.home');
+            return view('admin.home',compact('product'));
         }
         else 
         {
-            return view('home.userpage');
+            return view('home.userpage',compact('product'));
         }
     }
 }
